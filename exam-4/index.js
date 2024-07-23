9dd70eff9fe076c2f1488df532c7c4843495cd6e
ShreyMonpara01 <shreymonpara56@gmail.com>
2024-07-23 11:00:15 +0530

    exam-4

--- exam-4/index.js
+++ exam-4/index.js
@@ -1,33 +1,78 @@
-let users=[]
-const uiMaker = () => {
-      document.getElementById('tbody').innerHTML=""
-      for (let i = 0; i < users.length; i++) {
-        let tr = document.createElement('tr');
-        let td1 = document.createElement('td');
-        td1.innerHTML = users[i].name;
-        let td2 = document.createElement('td');
-        td2.innerHTML = users[i].grid;
-        let td3 = document.createElement('td');
-        td3.innerHTML = users[i].coures;
-        let td4 = document.createElement('td');
-        td4.innerHTML = users[i].number;
-        let td5 = document.createElement('td');
-        td5.innerHTML = users[i].fee;
-      
+let users = []
+let totalfees =Number(0);
+let totacourese =Number(0);
 
-        tr.append(td1, td2, td3, td4, td5)
-        document.getElementById('tbody').append(tr)
+const studentdelet = (index) => {
+    users.splice(index,1)
+    uimaker();
+}
+// delete all
+const DeleteAll = () => {
 
+    users=[];   
+  
+    document.getElementById('tbody').innerHTML = "";
+    document.getElementById("totalfees").innerHTML = 0;
+    document.getElementById("totacourese").innerHTML = 0;
+  
+    uiMaker();
+  }
+
+let uimaker = () => {
+    document.getElementById("tbody").innerHTML = ""
+    for (let i = 0; i < users.length; i++) {
+        let tr = document.createElement("tr")
+       // sr no.
+        let td = document.createElement("td")
+        td.innerHTML=i+1
+
+        let td1 = document.createElement("td")
+        td1.innerHTML = users[i].name
+        let td2 = document.createElement("td")
+        td2.innerHTML = users[i].Grid
+        let td3 = document.createElement("td")
+        td3.innerHTML = users[i].Course
+        let td4 = document.createElement("td")
+        td4.innerHTML = users[i].Fees
+        let td5 = document.createElement("td")
+        td5.innerHTML = users[i].Number
+
+        let td6 = document.createElement("td")
+        td6.innerHTML="Remove"
+        td6.addEventListener("click", () => studentdelet(i))
+
+        tr.append(td1, td2, td3, td4, td5, td6)
+        document.getElementById("tbody").append(tr)
     }
 }
-const data = (e) => {
+
+const studentdata = (e) => {
     e.preventDefault();
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("grid").value;
-    let department = document.getElementById("department").value;
-    let expireance = document.getElementById("expireance").value
-    let s_no
-    let level
-    let salary = document.getElementById("salary").value
+    let name = document.getElementById('name').value;
+    let Grid = document.getElementById('Grid').value;
+    let Course = document.getElementById('Course').value;
+    let Fees = document.getElementById('Fees').value;
+    let Number = document.getElementById('Number').value;
+
+
+    let user = {
+        name: name,
+        Grid: Grid,
+        Course: Course,
+        Fees: Fees,
+        Number: Number
+    }
+
+    users.push(user);
+    console.log(users);
+    uimaker();
+    
+  totalsalary+= Number(Fees);
+  totalemp+=1;
+
+  document.getElementById("totalfees").innerHTML = `totalfees : ${totalfees}`;
+  document.getElementById("totacourese").innerHTML = `totacourese : ${totacourese}`;
 }
+
+document.getElementById('studentdata').addEventListener("submit", studentdata)
