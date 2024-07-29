dc76b5eccac6e64319d3907f19cd70c45d6bf334
ShreyMonpara01 <shreymonpara56@gmail.com>
2024-07-29 20:09:02 +0530

    5 Count Occurrences of an Element.js

--- exam-5/Question 5 Count Occurrences of an Element/Question 5 Count Occurrences of an Element.js
+++ exam-5/Question 5 Count Occurrences of an Element/Question 5 Count Occurrences of an Element.js
@@ -1,13 +1,18 @@
 const Occurrencesarray = () => {
   // array
   let array = [1, 2, 2, 3, 2, 4, 5];
+  // get
   let get = 2;
+  // count
   let count = 0;
+  // for in if or array
   for (let i = 0; i <= array.length; i++) {
     if (array[i] === get) {
         count++;
     }
+    
 }
 console.log(count);
 };
+
 Occurrencesarray()
