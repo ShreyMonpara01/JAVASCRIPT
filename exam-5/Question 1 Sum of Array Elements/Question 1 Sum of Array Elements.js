11e8201b9f4300f8dc2320ba76d2b041e9aa25b0
ShreyMonpara01 <shreymonpara56@gmail.com>
2024-07-29 19:39:32 +0530

    exam-5

---
+++ exam-5/Question 1 Sum of Array Elements/Question 1 Sum of Array Elements.js
@@ -0,0 +1,15 @@
+let a = [1, 2, 3, 4, 5]
+const sum = () => {
+    for (let i = 0; i < arr.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+}
+// function sumArray(a) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += a[i];
+//     }
+//     return sum;
+// }
+1
---
+++ exam-5/Question 2 Find the Largest Element/Question 2 Find the Largest Element.js
@@ -0,0 +1,9 @@
+let arr =[1,2,3,4,5]
+const lelement =() =>{
+    let lelement =arr[i]
+
+    for(let i =0 ; i <arr .length; i++){
+        if(lelement  > arr[i])
+            lelement
+    }
+}
